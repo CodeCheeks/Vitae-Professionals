1c3a501243e0e84aa0727c38df80582c56af2c99
Alicia Parra <alicia@redparra.com>
2021-04-18 22:14:19 +0200

    Do login and authentication

---
+++ src/components/Views/PersonalArea/PersonalArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PersonalArea = () => {
+    return (
+        <div className='PersonalArea'>
+           <h1>Area Personal</h1>
+        </div>
+    );
+};
+
+export default PersonalArea;
