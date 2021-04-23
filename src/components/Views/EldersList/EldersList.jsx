96250d4dc9ede2a4a2edf5caf1895bc4649f5734
Alicia Parra <alicia@redparra.com>
2021-04-23 16:58:57 +0200

    Add styles

--- src/components/Views/EldersList/EldersList.jsx
+++ src/components/Views/EldersList/EldersList.jsx
@@ -18,9 +18,9 @@ const EldersList = () => {
     return (
         <div className='EldersList'>
             <div className="container">
-                <div className='row border'>
-                    <h5 className=' col-2 list__items'>Elder Name</h5>
-                    <h5 className='col-1 list__items'>Group</h5>
+                <div className='row border d-flex align-items-center justify-content-center'>
+                    <h5 className=' col-2 list__items '>Elder Name</h5>
+                    <h5 className='col-1 list__items '>Group</h5>
                     <h5 className=' col-2 list__items'>Birthday</h5>
                     <h5 className=' col-2 list__items'>Diet</h5>
                     <h5 className=' col-1 list__items'>Relative</h5>
@@ -31,14 +31,14 @@ const EldersList = () => {
                 (elders.map(elder => {
                     return <div key={elder.id}>
                             <div className='container'>
-                                <div className='row border'>
+                                <div className='row border d-flex align-items-center justify-content-center'>
                                     <Link to={`/elders/${elder.id}`} className='col-2 list__items'>{elder.firstname} {elder.lastname}</Link>
                                     <p className={groupColor(elder.group)}>⬤</p>
                                     <p className=' col-2 list__items'>{elder.dateOfBirth.split('T')[0].split("-").reverse().join("-")}</p>
                                     <p className=' col-2 list__items'>{elder.diet}</p>
                                     <p className=' col-1 list__items'>{elder.relative.firstname} </p>
                                     <p className=' col-3 list__items'>{elder.relative.phonenumber}</p>
-                                
+                                    
                                 </div>
                             </div>
 
