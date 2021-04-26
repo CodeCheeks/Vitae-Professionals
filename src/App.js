88d6c6c576ba7688514d4dc585920340a96ff9e8
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-26 18:50:01 +0200

    fix navbar in login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useContext } from 'react';
 import './App.css';
 import AppRouter from './components/AppRouter/AppRouter'
 import CustomFooter from './components/CustomFooter/CustomFooter';
 import CustomNavbar from './components/CustomNavbar/CustomNavbar'
+import { UserContext } from "./contexts/UserContext.jsx";
 
 function App() {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="App">
       <header>
-        <CustomNavbar></CustomNavbar>
-        <div className="custom__navbar__spacer"></div>
+        {user && <CustomNavbar/>}
+        {user && <div className="custom__navbar__spacer"></div>}
       </header>
 
       <main>
