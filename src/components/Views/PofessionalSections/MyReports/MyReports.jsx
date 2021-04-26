0cd00b60a05bf57878e8d854c3c3ba7a86301494
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-04-26 21:20:59 +0200

    Add reports views

--- src/components/Views/PofessionalSections/MyReports/MyReports.jsx
+++ src/components/Views/PofessionalSections/MyReports/MyReports.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './MyReports.css'
+import { UserContext } from "../../../../contexts/UserContext";
+import {deleteReports} from '../../../../services/ReportsService'
 
 const MyReports = () => {
+
+    const { user } = useContext(UserContext);
+
+    const editHandler = (event) => {
+        console.log("click edit")
+       
+    }
+
+    const deleteHandler = (report) => {
+        console.log("click delete")
+        //deleteReports(report.id)
+        
+    }
+
+    const getReports = () => {
+        let reports = []
+        user.reports.forEach(report => {
+            reports.push(
+                <div key={report} className="container">
+                    <div className="row border">
+                        <div className="col-10 align-self-center">
+                            <h5>{report}</h5>
+                        </div>
+                        <div className="col-2">
+                            <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619462590/Vitae/iconos/editar_u5y3fw.png" alt="edit" className="mx-3 custom__img" width="20px" onClick={editHandler}/>
+                            <img src="https://res.cloudinary.com/dv7hswrot/image/upload/v1619462590/Vitae/iconos/borrar_eoyvyu.png" alt="delete" className="mx-3 custom__img" width="20px" onClick={deleteHandler}/>
+                        </div>  
+                    </div>
+                </div>
+            )
+        })
+        return reports
+    }
+
+
     return (
-        <div ClassName='MyReports'>
-           <h1>My reports</h1> 
+        <div className="container">
+            <h1>Informes</h1>
+            <h3>{getReports()}</h3>
         </div>
     );
 };
 
 export default MyReports;
+
+
+
