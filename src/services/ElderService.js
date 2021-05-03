f8c94fe850111d17310a8ec2fca322f4d8522b18
Diego Cerratos Bravo <diegocerratos@gmail.com>
2021-05-03 21:09:34 +0200

    Add pictures

--- src/services/ElderService.js
+++ src/services/ElderService.js
@@ -21,6 +21,14 @@ export const deleteElder = (elder_id) => {
     return http.delete('/deleteElder', { params: { id: elder_id } })
 }
 
-export const editElder = (_id,body) => {
+export const editElder = (_id, body) => {
     return http.put(`/editElder/${_id}`, body)
   }
+  
+export const elderImages = (body, elder_id) => {
+  console.log(body)
+    return http.put(`/elderImages/${elder_id}`, body)
+  }
+
+
+  
